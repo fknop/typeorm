5e2074075dade8383efb9926afbfd4243c5840f6
Florian <6775689+fknop@users.noreply.github.com>
2022-12-31 19:20:23 +0100

    remove custom metadata

--- src/metadata-args/CustomDatabaseObjectArgs.ts
+++
@@ -1,8 +0,0 @@
-export interface CustomDatabaseObjectArgs {
-    /**
-     * Class to which table is applied.
-     * Function target is a table defined in the class.
-     * String target is a table defined in a json schema.
-     */
-    target: Function | string
-}
